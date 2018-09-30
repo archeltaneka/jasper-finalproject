e3be8b1044b55d554079e1e1a2eba0cb4091722c
Ikhwan Fikri Nur Afra <ikhwan.fikri@yahoo.co.id>
2018-09-30 10:09:17 +0700

    Fix the alarm module
    Copying the origin code and modify it

--- client/modules/AlarmPi.py
+++ client/modules/AlarmPi.py
@@ -1,94 +1,145 @@
-import datetime
+# -*- coding: utf-8-*-
+import random
 import re
+from datetime import datetime, time,timedelta
+from phue import Bridge
 import os
-from client.app_utils import getTimezone
-from semantic.dates import DateService
-from word2number import w2n 
+import glob
+import sys
+from word2number import w2n
 
 WORDS = []
 
-def words_to_hour(word):
+#def wordToInt(word):
+#
+#    value = ""
+#
+#    if "one" in word.lower():
+#        value = "1"
+#    elif "two" in word.lower():
+#        value = "2"
+#    elif "three" in word.lower():
+#        value = "3"
+#    elif "four" in word.lower():
+#        value = "4"
+#    elif "five" in word.lower():
+#        value = "5"
+#    elif "six" in word.lower():
+#        value = "6"
+#    elif "seven" in word.lower():
+#        value = "7"
+#    elif "eight" in word.lower():
+#        value = "8"
+#    elif "nine" in word.lower():
+#        value = "9"
+#    elif "ten" in word.lower():
+#        value = "10"
+#    elif "eleven" in word.lower():
+#        value = "11"
+#   elif "twelve" in word.lower():
+#        value = "12"
+#    else :
+#	int_val = int(re.search(r'\d+', word).group())      
+#	value = str(int_val)
+#
+#    if "pm" in word.lower() or "p.m." in word.lower():
+#        valueInt = int(value)
+#        valueInt += 12
+#        value = str(valueInt)
+#        
+#
+#    return value
 
-##    HOURS = {'ZERO':0, 'ONE':1, 'TWO':2, 'THREE':3, 'FOUR':4, 'FIVE':5,
-##             'SIX': 6, 'SEVEN':7, 'EIGHT':8, 'NINE':9, 'TEN':10,
-##             'ELEVEN':11, 'TWELVE':12, 'THIRTEEN':13, 'FOURTEEN':14, 'FIFTEEN':15,
-##             'SIXTEEN':16, 'SEVENTEEN':17, 'EIGHTEEN':18, 'NINETEEN':19, 'TWENTY':20,
-##             'TWENTY ONE':21, 'TWENTY TWO': 22, 'TWENTY THREE':23}
-##
-##    value = 0
-##    for hours in HOURS:
-##        if hours == word:
-##            return 
-    
-##    value = ""
-##
-##    if "one" in word.lower():
-##        value = "1"
-##    elif "two" in word.lower():
-##        value = "2"
-##    elif "three" in word.lower():
-##        value = "3"
-##    elif "four" in word.lower():
-##        value = "4"
-##    elif "five" in word.lower():
-##        value = "5"
-##    elif "six" in word.lower():
-##        value = "6"
-##    elif "seven" in word.lower():
-##        value = "7"
-##    elif "eight" in word.lower():
-##        value = "8"
-##    elif "nine" in word.lower():
-##        value = "9"
-##    elif "ten" in word.lower():
-##        value = "10"
-##    elif "eleven" in word.lower():
-##        value = "11"
-##    elif "twelve" in word.lower():
-##        value = "12"
-##    else :
-##	int_val = int(re.search(r'\d+', word).group())      
-##	value = str(int_val)
-##
-##    if "pm" in word.lower() or "p.m." in word.lower():
-##        valueInt = int(value)
-##        valueInt += 12
-##        value = str(valueInt)
-##        
-##    return value
+def handle(text, mic, profile):
+    messages1 = ["Naturally Sir ","Of course Sir "]
 
-def words_to_minute(word):
+    final = random.choice(messages1)
+    mic.say(final)
 
-    MINUTES = {}
+    cronString = 'echo "'
 
-    value = 0
-    for minute in MINUTES:
-        if minute == word:
-            return minute
-        else:
-            return 0
+    text.replace("at  ","",1)
+    global weekdayString
+    global hour
+    global hourString
 
+    if "every" in text.lower():
 
-def handle(text,mic,profile):
-    mic.say("what hour?")
-    first = mic.activeListen()
-    mic.say("what minute?")
-    second = mic.activeListen()
-    hour = w2n.word_to_num(first)
-    minute = w2n.word_to_num(second)
-    cronstring = 'echo "'
-    cronstring += minute + hour+ " * * 3" # belom lengkap
-    if hour > 12:
+        if "monday" in text.lower():
+            text.replace("monday","",1)
+            hour = wordToInt(text)
+            cronString += "0 "+hour+" * * 1"
+	        weekdayString = "Monday"
+
+        elif "tuesday" in text.lower():
+            text.replace("tuesday","",1)
+            hour = wordToInt(text)
+            cronString += "0 "+hour+" * * 2"
+	        weekdayString = "Tuesday"
+
+        elif "wednesday" in text.lower():
+            text.replace("wednesday","",1)
+            hour = wordToInt(text)
+            cronString += "0 "+hour+" * * 3"
+            weekdayString = "Wednesday"
+
+        elif "thursday" in text.lower():
+            text.replace("thursday","",1)
+            hour = wordToInt(text)
+            cronString += "0 "+hour+" * * 4"
+            weekdayString = "Thursday"
+
+        elif "friday" in text.lower():
+            text.replace("friday","",1)
+            hour = wordToInt(text)
+            cronString += "0 "+hour+" * * 5"
+            weekdayString = "Friday"
+
+        elif "saturday" in text.lower():  
+            text.replace("saturday","",1)
+            hour = wordToInt(text)
+            cronString += "0 "+hour+" * * 6"
+            weekdayString = "Saturday"
+
+        elif "sunday" in text.lower():
+            text.replace("sunday","",1)
+            hour = wordToInt(text)
+            cronString += "0 "+hour+" * * 0"
+            weekdayString = "Sunday"
+
+	else :
+	    sys.exit(1)
+
+    	cronString += ' /home/philipp/.jasper/alarmScript.sh" | tee -a /var/spool/cron/philipp'
+	print("cd /home/philipp &&"+cronString)
+
+    	if hour > 12:
             hourInt = int(hour)
             hourInt = hourInt-12
-            hourString = str(hourInt)+" pm"
-    else:
-            hourString = str(hour)+ " am"
-    
-    mic.say("Setting alarm for " + " at " + hourString)
-    os.system("cd /home/raspberry &&"+cronstring)
-    
-    
+            hourString += str(hourInt)+" pm"
+    	else:
+            hourString +=str(hour)+ " am"
+
+    	mic.say("Setting alarm for " + weekdayString +" at "+ hourString)
+
+    	os.system("cd /home/philipp &&"+cronString)
+
+
+    elif "in" in text.lower() and ("hours" in text.lower() or "hour" in text.lower()):
+	hour = wordToInt(text)
+	command ='echo "/home/philipp/.jasper/jamesAlarm.py" |at now + ' 
+	command += hour
+	command += " hours"
+	print(command)
+	os.system(command)
+	
+	xHoursFromNow = datetime.now() + timedelta(hours=int(hour))
+	mic.say("I set your alarm for "+ str(xHoursFromNow.hour)+" "+ str(xHoursFromNow.minute)+". ")
+	
+
+
+
+
 
 def isValid(text):
-    return bool(re.search(r'\b(set|add) alarm\b)', text, re.IGNORECASE))
+    return bool(re.search(r'\b(((add|set) (a|another|an) (alarm|clock)|wake me))\b', text, re.IGNORECASE))
