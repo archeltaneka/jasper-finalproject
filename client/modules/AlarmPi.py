14410785bbe34c750c543da61d2cb1cca7304c64
Ikhwan Fikri Nur Afra <ikhwan.fikri@yahoo.co.id>
2018-09-30 16:52:34 +0700

    Fix the alarm module
    Finally Pass the Syntax analysis based on Python 3.7

--- client/modules/AlarmPi.py
+++ client/modules/AlarmPi.py
@@ -2,7 +2,6 @@
 import random
 import re
 from datetime import datetime, time,timedelta
-from phue import Bridge
 import os
 import glob
 import sys
@@ -65,76 +64,76 @@ def handle(text, mic, profile):
 
     if "every" in text.lower():
 
-        if "monday" in text.lower():
-            text.replace("monday","",1)
-            hour = wordToInt(text)
-            cronString += "0 "+hour+" * * 1"
-	        weekdayString = "Monday"
+            if "monday" in text.lower():
+                    text.replace("monday","",1)
+                    hour = w2n.word_to_num(text)
+                    cronString += "0 "+hour+" * * 1"
+                    weekdayString = "Monday"
 
-        elif "tuesday" in text.lower():
-            text.replace("tuesday","",1)
-            hour = wordToInt(text)
-            cronString += "0 "+hour+" * * 2"
-	        weekdayString = "Tuesday"
+            elif "tuesday" in text.lower():
+                    text.replace("tuesday","",1)
+                    hour = w2n.word_to_num(text)
+                    cronString += "0 "+hour+" * * 2"
+                    weekdayString = "Tuesday"
 
-        elif "wednesday" in text.lower():
-            text.replace("wednesday","",1)
-            hour = wordToInt(text)
-            cronString += "0 "+hour+" * * 3"
-            weekdayString = "Wednesday"
+            elif "wednesday" in text.lower():
+                    text.replace("wednesday","",1)
+                    hour = w2n.word_to_num(text)
+                    cronString += "0 "+hour+" * * 3"
+                    weekdayString = "Wednesday"
 
-        elif "thursday" in text.lower():
-            text.replace("thursday","",1)
-            hour = wordToInt(text)
-            cronString += "0 "+hour+" * * 4"
-            weekdayString = "Thursday"
+            elif "thursday" in text.lower():
+                    text.replace("thursday","",1)
+                    hour = w2n.word_to_num(text)
+                    cronString += "0 "+hour+" * * 4"
+                    weekdayString = "Thursday"
 
-        elif "friday" in text.lower():
-            text.replace("friday","",1)
-            hour = wordToInt(text)
-            cronString += "0 "+hour+" * * 5"
-            weekdayString = "Friday"
+            elif "friday" in text.lower():
+                    text.replace("friday","",1)
+                    hour = w2n.word_to_num(text)
+                    cronString += "0 "+hour+" * * 5"
+                    weekdayString = "Friday"
 
-        elif "saturday" in text.lower():  
-            text.replace("saturday","",1)
-            hour = wordToInt(text)
-            cronString += "0 "+hour+" * * 6"
-            weekdayString = "Saturday"
+            elif "saturday" in text.lower():  
+                    text.replace("saturday","",1)
+                    hour = w2n.word_to_num(text)
+                    cronString += "0 "+hour+" * * 6"
+                    weekdayString = "Saturday"
 
-        elif "sunday" in text.lower():
-            text.replace("sunday","",1)
-            hour = wordToInt(text)
-            cronString += "0 "+hour+" * * 0"
-            weekdayString = "Sunday"
+            elif "sunday" in text.lower():
+                    text.replace("sunday","",1)
+                    hour = w2n.word_to_num(text)
+                    cronString += "0 "+hour+" * * 0"
+                    weekdayString = "Sunday"
 
-	else :
-	    sys.exit(1)
+            else:
+                    sys.exit(1)
 
-    	cronString += ' /home/raspberry/.jasper/alarmScript.sh" | tee -a /var/spool/cron/philipp'
-	print("cd /home/raspberry &&"+cronString)
+            cronString += ' /home/pi/.jasper/alarmScript.sh" | tee -a /var/spool/cron/pi'
+            print("cd /home/pi && "+cronString)
 
-    	if hour > 12:
-            hourInt = int(hour)
-            hourInt = hourInt-12
-            hourString += str(hourInt)+" pm"
-    	else:
-            hourString +=str(hour)+ " am"
+            if hour > 12:
+                hourInt = int(hour)
+                hourInt = hourInt-12
+                hourString = str(hourInt)+" pm"
+            else:
+                hourString =str(hour)+ " am"
 
-    	mic.say("Setting alarm for " + weekdayString +" at "+ hourString)
+            mic.say("Setting alarm for " + weekdayString +" at "+ hourString)
 
-    	os.system("cd /home/raspberry &&"+cronString)
+            os.system("cd /home/pi && "+cronString)
 
 
     elif "in" in text.lower() and ("hours" in text.lower() or "hour" in text.lower()):
-	hour = wordToInt(text)
-	command ='echo "/home/raspberry/.jasper/jamesAlarm.py" |at now + ' 
-	command += hour
-	command += " hours"
-	print(command)
-	os.system(command)
-	
-	xHoursFromNow = datetime.now() + timedelta(hours=int(hour))
-	mic.say("I set your alarm for "+ str(xHoursFromNow.hour)+" "+ str(xHoursFromNow.minute)+". ")
+        hour = w2n.word_to_num(text)
+        command ='echo "/home/pi/.jasper/jamesAlarm.py" |at now + ' 
+        command += hour
+        command += " hours"
+        print(command)
+        os.system(command)
+        
+        xHoursFromNow = datetime.now() + timedelta(hours=int(hour))
+        mic.say("I set your alarm for "+ str(xHoursFromNow.hour)+" "+ str(xHoursFromNow.minute)+". ")
 	
 
 
